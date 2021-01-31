0f2b7d5f4c613704602c5fc7f22e6f418e9050bd
kosharskiy <vetalkosharskiy@gmail.com>
2021-01-31 23:00:06 +0200

    implemented dialogs for edit params values

---
+++ app/src/main/java/org/secu3/android/ui/parameters/dialogs/ParamBaseEditDialogFragment.kt
@@ -0,0 +1,54 @@
+/* SecuDroid  - An open source, free manager for SECU-3 engine control unit
+   Copyright (C) 2020 Vitaliy O. Kosharskiy. Ukraine, Kharkiv
+
+   SECU-3  - An open source, free engine control unit
+   Copyright (C) 2007 Alexey A. Shabelnikov. Ukraine, Kyiv
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+   contacts:
+              http://secu-3.org
+              email: vetalkosharskiy@gmail.com
+*/
+package org.secu3.android.ui.parameters.dialogs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import org.secu3.android.databinding.FragmentParamsEditDialogBinding
+
+
+const val ARG_CURRENT_VALUE = "current_value"
+const val ARG_PARAM_TITLE = "param_title"
+const val ARG_STEP_VALUE = "step_value"
+
+
+abstract class ParamBaseEditDialogFragment : DialogFragment() {
+
+    protected lateinit var mBinding: FragmentParamsEditDialogBinding
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        mBinding = FragmentParamsEditDialogBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+}
