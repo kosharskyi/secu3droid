3ef6645a10c098a00e000bc1f6667cb264773196
kosharskiy <vetalkosharskiy@gmail.com>
2021-02-02 21:23:39 +0200

    implemented custom views for number params

---
+++ app/src/main/java/org/secu3/android/ui/parameters/views/IntParamView.kt
@@ -0,0 +1,54 @@
+package org.secu3.android.ui.parameters.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import org.secu3.android.R
+import org.secu3.android.databinding.ViewBaseParameterBinding
+
+class IntParamView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private lateinit var mBinding: ViewBaseParameterBinding
+
+    var title: String = ""
+        set(value) {
+            field = value
+            mBinding.paramTitle.text = value
+        }
+
+    var units: String = ""
+        set(value) {
+            field = value
+            mBinding.paramUnits.text = value
+        }
+
+    var value: Int = 0
+        set(value) {
+            field = value
+            mBinding.paramValue.text = value.toString()
+        }
+
+    init {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.view_base_parameter, this, true)
+        mBinding = ViewBaseParameterBinding.bind(this)
+
+        with(TypedValue()) {
+            context.theme.resolveAttribute(android.R.attr.selectableItemBackground, this, true)
+            setBackgroundResource(resourceId)
+        }
+
+        context.obtainStyledAttributes(attrs, R.styleable.IntParamView).apply {
+            getString(R.styleable.IntParamView_title)?.let { title = it }
+            getString(R.styleable.IntParamView_units)?.let { units = it }
+            value = getInt(R.styleable.IntParamView_int_value, 0)
+
+            recycle()
+        }
+    }
+
+}
