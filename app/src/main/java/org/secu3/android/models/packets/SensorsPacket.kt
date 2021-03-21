877d52bd3490ad76a344139abb1a78c4af943dab
kosharskiy <vetalkosharskiy@gmail.com>
2021-03-21 19:19:38 +0200

    fixed sensor packet parser

--- app/src/main/java/org/secu3/android/models/packets/SensorsPacket.kt
+++ app/src/main/java/org/secu3/android/models/packets/SensorsPacket.kt
@@ -142,7 +142,7 @@ data class SensorsPacket(var rpm: Int = 0,
             addI2 = data.get2Bytes(22).toFloat() / VOLTAGE_MULTIPLIER
 
             ecuErrors = data.get2Bytes(24).toShort()
-            chokePosition = data[26].toInt().toFloat()
+            chokePosition = data[26].toInt().toFloat() / CHOKE_MULTIPLIER
             gasDosePosition = data[27].toInt() / GAS_DOSE_MULTIPLIER
 
             rawSpeed = data.get2Bytes(28)
