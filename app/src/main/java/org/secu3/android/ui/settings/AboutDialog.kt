417079bfd2d2f88bc17d0f52c080a7e96ddf0338
kosharskiy <vetalkosharskiy@gmail.com>
2024-01-01 14:08:59 +0200

    Updated about dialog

--- app/src/main/java/org/secu3/android/ui/settings/AboutDialog.kt
+++ app/src/main/java/org/secu3/android/ui/settings/AboutDialog.kt
@@ -1,26 +1,27 @@
-/* SecuDroid  - An open source, free manager for SECU-3 engine control unit
-   Copyright (C) 2020 Vitaliy O. Kosharskiy. Ukraine, Kharkiv
-
-   SECU-3  - An open source, free engine control unit
-   Copyright (C) 2007 Alexey A. Shabelnikov. Ukraine, Kyiv
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation, either version 3 of the License, or
-   (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-
-   You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-   contacts:
-              http://secu-3.org
-              email: vetalkosharskiy@gmail.com
-*/
+/*
+ *    SecuDroid  - An open source, free manager for SECU-3 engine control unit
+ *    Copyright (C) 2024 Vitaliy O. Kosharskyi. Ukraine, Kyiv
+ *
+ *    SECU-3  - An open source, free engine control unit
+ *    Copyright (C) 2007-2024 Alexey A. Shabelnikov. Ukraine, Kyiv
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    contacts:
+ *                    http://secu-3.org
+ *                    email: vetalkosharskiy@gmail.com
+ */
 package org.secu3.android.ui.settings
 
 import android.os.Bundle
@@ -30,7 +31,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import org.secu3.android.BuildConfig
+import org.secu3.android.R
 import org.secu3.android.databinding.AboutBinding
+import org.threeten.bp.LocalDateTime
 
 class AboutDialog : DialogFragment() {
 
@@ -46,13 +49,19 @@ class AboutDialog : DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mBinding.aboutLicense.movementMethod = LinkMovementMethod.getInstance()
-        val defaultColor: Int = mBinding.aboutLicense.textColors.defaultColor
-        mBinding.aboutLicense.setTextColor(defaultColor)
+        mBinding.apply {
+            aboutLicense.movementMethod = LinkMovementMethod.getInstance()
+            val defaultColor: Int = aboutLicense.textColors.defaultColor
+            aboutLicense.setTextColor(defaultColor)
 
-        mBinding.aboutSources.setTextColor(defaultColor)
+            aboutSources.setTextColor(defaultColor)
 
-        mBinding.aboutVersionName.text = BuildConfig.VERSION_NAME
+            aboutVersionName.text = BuildConfig.VERSION_NAME
+
+            val year = LocalDateTime.now().year
+            copyright1.text = getString(R.string.about_copyright_1, year)
+            copyright2.text = getString(R.string.about_copyright_2, year)
+        }
     }
 
 
