2e1f3b7b91776664ae155e216ad04539543e1fa9
kosharskiy <vetalkosharskiy@gmail.com>
2024-01-07 20:47:13 +0200

    added setup.bat script

---
+++ setup.bat
@@ -0,0 +1,4 @@
+echo KEY_ALIAS=debug > keystore.properties
+echo KEY_PASSWORD=debug >> keystore.properties
+echo KEYSTORE_PASSWORD=debug >> keystore.properties
+echo KEYSTORE_PATH=debug.jks >> keystore.properties
