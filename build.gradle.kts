372266ae96c02948ee67066625f16c672f9bc0b6
kosharskyi <kosharskiy@ukr.net>
2026-09-16 22:29:37 +0300

    Update Gradle and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("com.android.application") version "9.3.1" apply false
+    id("com.android.application") version "9.4.0" apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.navigation.safeargs) apply false
