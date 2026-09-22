63957f3d454e374ad102b98462aa76501c9886cd
kosharskyi <kosharskiy@ukr.net>
2026-09-22 12:58:09 +0300

    Build #57

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ val keystorePropertiesFile = rootProject.file("keystore.properties")
 val keystoreProperties = Properties()
 val hasReleaseKeystore = keystorePropertiesFile.exists()
 val isReleaseTask = gradle.startParameter.taskNames.any { it.lowercase().contains("release") }
-val appVersionCode = 56
-val appVersionName = "0.19.0"
+val appVersionCode = 57
+val appVersionName = "0.19.1"
 
 if (hasReleaseKeystore) {
     keystoreProperties.load(FileInputStream(keystorePropertiesFile))
